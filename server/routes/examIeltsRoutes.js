df9b8b898f28141c6858de2da3e1bc82ca92da4f
SahanChamara <sahanchamara456@gmail.com>
2025-07-01 14:40:51 +0530

    feat: Add exam management routes and controller for IELTS

---
+++ server/routes/examIeltsRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const examIeltsController = require("../controllers/examIeltsController");
+const authMiddleware = require("../middleware/auth");
+
+router.post('/exam', authMiddleware(["Instructor", "SuperAdmin"]), examIeltsController.);
+router.post('/sections', authMiddleware(["Instructor", "SuperAdmin"]), examIeltsController.);
+router.post('/questions', authMiddleware(["Instructor", "SuperAdmin"]), examIeltsController.);
+router.get('/', authMiddleware(["Student", "Instructor", "SuperAdmin"]), examIeltsController.);
+router.delete('/:examId', authMiddleware(["Instructor", "SuperAdmin"]), examIeltsController.);
+router.put('/:examId', authMiddleware(["Instructor", "SuperAdmin"]), examIeltsController.);
+
+
+module.exports = router;
