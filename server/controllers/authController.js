d16dbcadf8e4ce9fa37ca567af1f7370457b6e47
SahanChamara <sahanchamara456@gmail.com>
2025-07-16 12:08:13 +0530

    fix: Remove commented-out token generation and cookie handling code for cleaner authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -82,25 +82,6 @@ const register = async (req, res) => {
         await user.save();
 
         const payload = {id: user._id, role};
-        /*const token = jwt.sign(payload, process.env.JWT_SECRET, {
-            expiresIn: "1h",
-        });*/
-        /*const accessToken = generateToken(payload, "15m");
-        const refreshToken = generateToken(payload, "7d");
-
-        res.cookie("accessToken", accessToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            maxAge:  15 * 60 * 1000         //15min                                   //24 * 60 * 60 * 1000
-        });
-
-        res.cookie("refreshToken", refreshToken, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-        });*/
 
         res.success(
             {
