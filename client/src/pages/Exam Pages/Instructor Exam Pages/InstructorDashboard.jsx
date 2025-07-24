f75d07cb76dfbb5a5e39ff76f4182995b12bc9bb
SahanChamara <sahanchamara456@gmail.com>
2025-07-24 09:47:18 +0530

    fix: Remove invalid placeholder text from handleCreateExam function in InstructorDashboard

--- client/src/pages/Exam Pages/Instructor Exam Pages/InstructorDashboard.jsx
+++ client/src/pages/Exam Pages/Instructor Exam Pages/InstructorDashboard.jsx
@@ -36,7 +36,7 @@ const InstructorDashboard = () => {
   const handleCreateExam = () => {
     setShowCreateForm(true);
   };
-  adasdasdawdsa
+  
 
   const handleEditExam = (exam) => {
     setSelectedExam(exam);
