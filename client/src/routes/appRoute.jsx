a17751afaffdceb68538a65c63bc1fff4cdbdd7f
SahanChamara <sahanchamara456@gmail.com>
2025-09-29 16:24:04 +0530

    Add SuperAdminUnitControl component and update routing for unit management

--- client/src/routes/appRoute.jsx
+++ client/src/routes/appRoute.jsx
@@ -50,7 +50,8 @@ import SuperAdminnotifications from '../pages/Adminpages/adminnotifications'
 import IELTSLandingPage from '../pages/Adminpages/landing'
 import AdminFeed from '../pages/Adminpages/AdminFeed'
 import InstructorDashboard from "../pages/Exam Pages/Instructor Exam Pages/InstructorDashboard";
-import CourseAdd from '../pages/Adminpages/courseadd'
+import CourseAdd from '../pages/Adminpages/courseadd';
+import SuperAdminUnitControl from '../pages/Adminpages/unitAdd'
 
 // Defining the application routes
 const AppRoutes = () => {
@@ -108,7 +109,7 @@ const AppRoutes = () => {
         <Route path='/notifications/admin' element={<SuperAdminnotifications />} />
         <Route path='/notifications/admin' element={<SuperAdminnotifications />} />
         <Route path='/course/admin' element={<CourseAdd/>} />
-        <Route path='/unit/admin' element={<AdminFeed/>} />
+        <Route path='/unit/admin' element={<SuperAdminUnitControl/>} />
       </Route>
     </Routes>
   )
