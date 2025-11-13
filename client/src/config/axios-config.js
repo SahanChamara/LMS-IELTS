6cdc1f6f56188ba883010f52a2346d6720f76d41
SahanChamara <sahanchamara456@gmail.com>
2025-11-13 14:23:17 +0530

    Update API_BASE_URL to use environment variable for better configuration management

--- client/src/config/axios-config.js
+++ client/src/config/axios-config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "../redux/store-config/store";
 import { logout, refreshTokenAPI } from "../redux/features/authSlice";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
